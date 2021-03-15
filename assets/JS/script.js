70c7e4ab77180690a54d63cfe3d116582892694a
Sheldon Stevens <sgste080@yahoo.com>
2021-03-14 19:55:34 -0700

    I f*& up

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,19 +1,16 @@
 // City
-var city = $("#search-input").val();
-var cityName = $("<h2>").text(response.name);
-var cityNameList = $("<li>").text(response.name);
-var cityTemp = $("<p class='temp'>").text(Math.floor(tempF) + "°");
-var cityHumidity = $("<p>").text("Humidity: " + response.main.humidity + "%");
-var cityWindSpeed = $("<p>").text("Wind Speed: " + response.wind.speed + " MPH");
+
 // Temp
-var tempInt = parseInt(response.main.temp);
-var tempF = (tempInt * 9 / 5) - 459.67;
+
 // Loc
-var lat = response.coord.lat;
-var lon = response.coord.lon;
+
+// Other
+
+// Date
+
+// Weather
+
 // U R ELLS
-var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=000401db107b5cbe165fdf198e9f1e47";
-var queryURL2 = "https://api.openweathermap.org/data/2.5/uvi?appid=000401db107b5cbe165fdf198e9f1e47&lat=" + lat + "&lon=" + lon;
 
 window.onload = () {
 
